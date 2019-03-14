6a199be5214fd6900c5bfef46c9ddb5b467886ad
Sergey Shandar <sergey.shandar@gmail.com>
2019-03-14 16:41:53 -0700

    Chaining

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,35 @@ export type Entry<T> = sync.Entry<T>
 
 export const entry = sync.entry
 
-export const iterable = <T>(createIterator: () => AsyncIterator<T>): AsyncIterable<T> =>
-  ({ [Symbol.asyncIterator]: createIterator })
+export type AsyncIterableEx<T> = {
+  readonly fold: <A>(func: (a: A, b: T, i: number) => A, init: A) => Promise<A>
+  readonly toArray: () => Promise<ReadonlyArray<T>>
+  readonly entries: () => AsyncIterableEx<Entry<T>>
+  readonly map: <R>(func: (v: T, i: number) => R) => AsyncIterableEx<R>
+  readonly flatMap: <R>(func: (v: T, i: number) => AsyncIterable<R>) => AsyncIterableEx<R>
+} & AsyncIterable<T>
 
-export const fromSync = <T>(input: sync.Iterable<T>): AsyncIterable<T> =>
+class AsyncIterableImpl<T> implements AsyncIterableEx<T> {
+  public readonly [Symbol.asyncIterator]: () => AsyncIterator<T>
+  constructor(createIterator: () => AsyncIterator<T>) {
+    this[Symbol.asyncIterator] = createIterator
+  }
+  public fold<A>(func: (a: A, b: T, i: number) => A, init: A) { return fold(this, func, init) }
+  public toArray() { return toArray(this) }
+  public entries() { return entries(this) }
+  public map<R>(func: (v: T, i: number) => R) { return map(this, func) }
+  public flatMap<R>(func: (v: T, i: number) => AsyncIterable<R>) { return flatMap(this, func) }
+}
+
+export const iterable = <T>(createIterator: () => AsyncIterator<T>): AsyncIterableEx<T> =>
+  new AsyncIterableImpl(createIterator)
+
+export const fromSync = <T>(input: sync.Iterable<T>): AsyncIterableEx<T> =>
   iterable(async function *(): AsyncIterator<T> { yield *input })
 
-export const fromSequence = <T>(...a: T[]): AsyncIterable<T> => fromSync(a)
+export const fromSequence = <T>(...a: T[]): AsyncIterableEx<T> => fromSync(a)
 
-export const fromPromise = <T>(p: Promise<sync.Iterable<T>>): AsyncIterable<T> =>
+export const fromPromise = <T>(p: Promise<sync.Iterable<T>>): AsyncIterableEx<T> =>
   iterable(async function *(): AsyncIterator<T> { yield *await p })
 
 export const fold = async <T, A>(
@@ -29,14 +49,14 @@ export const fold = async <T, A>(
   return result
 }
 
-export const toArray = <T>(input: AsyncIterable<T>): Promise<ReadonlyArray<T>> =>
+export const toArray = <T>(input: AsyncIterable<T>|undefined): Promise<ReadonlyArray<T>> =>
   fold(
     input,
     (a, i) => { a.push(i); return a },
     new Array<T>()
   )
 
-export const entries = <T>(input: AsyncIterable<T>|undefined): AsyncIterable<Entry<T>> =>
+export const entries = <T>(input: AsyncIterable<T>|undefined): AsyncIterableEx<Entry<T>> =>
   iterable(async function *(): AsyncIterator<sync.Entry<T>> {
     // tslint:disable-next-line:no-if-statement
     if (input === undefined) {
@@ -54,7 +74,7 @@ export const entries = <T>(input: AsyncIterable<T>|undefined): AsyncIterable<Ent
 export const map = <T, I>(
   input: AsyncIterable<I>|undefined,
   func: (v: I, i: number) => T,
-): AsyncIterable<T> =>
+): AsyncIterableEx<T> =>
   iterable(async function *(): AsyncIterator<T> {
     /* tslint:disable-next-line:no-loop-statement */
     for await (const [index, value] of entries(input)) {
@@ -62,7 +82,7 @@ export const map = <T, I>(
     }
   })
 
-export const flatten = <T>(input: AsyncIterable<AsyncIterable<T>|undefined>|undefined): AsyncIterable<T> =>
+export const flatten = <T>(input: AsyncIterable<AsyncIterable<T>|undefined>|undefined): AsyncIterableEx<T> =>
   iterable(async function *(): AsyncIterator<T> {
     // tslint:disable-next-line:no-if-statement
     if (input === undefined) {
@@ -80,5 +100,5 @@ export const flatten = <T>(input: AsyncIterable<AsyncIterable<T>|undefined>|unde
 export const flatMap = <T, I>(
   input: AsyncIterable<I>|undefined,
   func: (v: I, i: number) => AsyncIterable<T>,
-): AsyncIterable<T> =>
+): AsyncIterableEx<T> =>
     flatten(map(input, func))
